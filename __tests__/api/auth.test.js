d6ecbfe0faf20170a2635180d761a260d4408e8b
Jose Gustavo Garcia <jggarciago@gmail.com>
2021-10-05 15:56:08 -0500

    Fix auth integration test and polish login e2eTest

--- __tests__/api/auth.test.js
+++ __tests__/api/auth.test.js
@@ -1,7 +1,10 @@
 import { login, InvalidCredentialsError } from "../../api/auth";
-
+import fetch from "isomorphic-fetch";
 describe("API", () => {
-  it.skip("Returns an error message for invalid credentials", async () => {
+  beforeAll(() => {
+    global.fetch = fetch;
+  });
+  it("Returns an error message for invalid credentials", async () => {
     const email = "invalid@credentials.com",
       password = "incorrect horse battery staple";
     await expect(() => login(email, password)).rejects.toThrow(
@@ -10,12 +13,12 @@ describe("API", () => {
   });
 
   //Fetch does not work due to it being called locally instead of the browser
-  it.skip("Returns success, role and token with valid credentials", async () => {
+  it("Returns success, role and token with valid credentials", async () => {
     const email = "employee@ontime.com",
       password = "ontime";
     const response = await login(email, password);
-    expect(response["role"]).toBeTruthy();
-    expect(response["password"]).toBeTruthy();
+    expect(response["role"]).toBe("employee");
+    expect(response["accessToken"]).toBeTruthy();
   });
   it("Returns an error message for empty credentials", async () => {
     const email = "",
--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -4,29 +4,29 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+function typeCredentials(user) {
+  cy.get("input[type=email]").click().type(user["email"]);
+  cy.get("input[type=password]").click().type(user["password"]);
+  cy.findByRole('button', {name: /Iniciar/i}).click()
+}
+
 describe("Login", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("Logins admin correctly", () => {
-    cy.get("input[type=email]").click().type("admin@ontime.com");
-    cy.get("input[type=password]").click().type("ontime");
-    cy.get("[role=button]").click();
+    typeCredentials({email:"admin@ontime.com",password:"ontime"})
     cy.contains("administrador", { matchCase: false }).should("exist");
   });
 
   it("Logins employee correctly", () => {
-    cy.get("input[type=email]").click().type("employee@ontime.com");
-    cy.get("input[type=password]").click().type("ontime");
-    cy.get("[role=button]").click();
+    typeCredentials({email:"employee@ontime.com",password:"ontime"})
     cy.contains("empleado", { matchCase: false }).should("exist");
   });
 
   it("Logins employer correctly", () => {
-    cy.get("input[type=email]").click().type("employeer@ontime.com");
-    cy.get("input[type=password]").click().type("ontime");
-    cy.get("[role=button]").click();
+    typeCredentials({email:"employeer@ontime.com",password:"ontime"})
     //Find camera button
     cy.get(
       '[style="background-color: rgb(3, 218, 196); bottom: 96px; box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 6px; opacity: 1; right: 24px; transform: scale(1);"] > .css-cursor-18t94o4'
