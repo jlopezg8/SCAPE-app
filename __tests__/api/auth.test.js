960f05e7e882c5aaef43d88e5e64b6ab428f07fb
jlopezg8 <jlopezg8@gmail.com>
2021-10-05 22:29:24 -0500

    bit of cleaning before merging

--- __tests__/api/auth.test.js
+++ __tests__/api/auth.test.js
@@ -1,33 +1,36 @@
-import { login, InvalidCredentialsError } from "../../api/auth";
-import fetch from "isomorphic-fetch";
-describe("API", () => {
-  beforeAll(() => {
-    global.fetch = fetch;
+import fetch from 'isomorphic-fetch';
+
+import { InvalidCredentialsError, login } from '../../api/auth';
+import { RequiredArgumentError } from '../../api/errors';
+
+global.fetch = fetch;
+
+describe('auth API tests', () => {
+  it('returns an error message for invalid credentials', () => {
+    const email = 'invalid@credentials.com',
+          password = 'incorrect horse battery staple';
+    return (
+      expect(login(email, password)).rejects.toThrow(InvalidCredentialsError));
   });
-  it("Returns an error message for invalid credentials", async () => {
-    const email = "invalid@credentials.com",
-      password = "incorrect horse battery staple";
-    await expect(() => login(email, password)).rejects.toThrow(
-      InvalidCredentialsError
+
+  it('returns a role and an access token with valid credentials', async () => {
+    const email = 'employee@ontime.com', password = 'ontime';
+    const { role, accessToken } = await login(email, password);
+    expect(role).toBe('employee');
+    expect(accessToken).toBeTruthy();
+  });
+
+  it('returns an error message for empty credentials', () => {
+    const email = '', password = '';
+    return (
+      expect(login(email, password)).rejects.toThrow(RequiredArgumentError)
     );
   });
 
-  //Fetch does not work due to it being called locally instead of the browser
-  it("Returns success, role and token with valid credentials", async () => {
-    const email = "employee@ontime.com",
-      password = "ontime";
-    const response = await login(email, password);
-    expect(response["role"]).toBe("employee");
-    expect(response["accessToken"]).toBeTruthy();
-  });
-  it("Returns an error message for empty credentials", async () => {
-    const email = "",
-      password = "";
-    await expect(() => login(email, password)).rejects.toThrow();
-  });
-  it("Returns an error message for null credentials", async () => {
-    const email = null,
-      password = null;
-    await expect(() => login(email, password)).rejects.toThrow();
+  it('returns an error message for null credentials', async () => {
+    const email = null, password = null;
+    return (
+      expect(login(email, password)).rejects.toThrow(RequiredArgumentError)
+    );
   });
 });
--- __tests__/screens/LoginScreen.test.js
+++ __tests__/screens/LoginScreen.test.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import { AuthContext, InvalidCredentialsError } from "../../hooks/useAuth";
-import LoginScreen from "../../screens/LoginScreen";
-import { fireEvent, render } from "../../test-utils";
+import { AuthContext, InvalidCredentialsError } from '../../hooks/useAuth';
+import LoginScreen from '../../screens/LoginScreen';
+import { fireEvent, render } from '../../test-utils';
 
-describe("login screen tests", () => {
-  it("displays an error message for invalid credentials", async () => {
+describe('login screen tests', () => {
+  it('displays an error message for invalid credentials', async () => {
     const authValue = {
       login: jest.fn().mockRejectedValue(new InvalidCredentialsError()),
     };
@@ -14,34 +14,12 @@ describe("login screen tests", () => {
         <LoginScreen />
       </AuthContext.Provider>
     );
-    const email = "invalid@credentials.com",
-      password = "incorrect horse battery staple";
-    fireEvent.changeText(getByLabelText("Correo electrónico"), email);
-    fireEvent.changeText(getByLabelText("Contraseña"), password);
-    fireEvent.press(getByText("Iniciar sesión"));
-    await findByText("Usuario o contraseña incorrectos");
+    const email = 'invalid@credentials.com',
+      password = 'incorrect horse battery staple';
+    fireEvent.changeText(getByLabelText('Correo electrónico'), email);
+    fireEvent.changeText(getByLabelText('Contraseña'), password);
+    fireEvent.press(getByText('Iniciar sesión'));
+    await findByText('Usuario o contraseña incorrectos');
     expect(authValue.login).toHaveBeenCalledWith(email, password);
   });
 });
-
-/*import user from '@testing-library/user-event'
-import DrinkSearch from './DrinkSearch'
-import { mockServer } from './mocks/server.js'
-beforeAll(() => mockServer.listen())
-afterEach(() => mockServer.resetHandlers())
-afterAll(() => mockServer.close())
-test('renders mock drink data, async () => {
-  render(<DrinkSearch />)
-  const searchInput = screen.getByRole('searchbox')
-  user.type(searchInput, 'vodka, {enter}')
-  expect(
-    await screen.findByRole('img', { name: /test drink/i })
-  ).toBeInTheDocument()
-  expect(
-    screen.getByRole('heading', { name: /test drink/i })
-    ).toBeInTheDocument()
-  expect(screen.getByText(/test 
-    ingredient/i)).toBeInTheDocument()
-  expect(screen.getByText(/test 
-    instructions/i)).toBeInTheDocument()
-})*/
