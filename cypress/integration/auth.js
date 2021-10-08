6ab137f1bb64dbb848cd34647030baf0957750d7
Jose Gustavo Garcia <jggarciago@gmail.com>
2021-10-08 09:10:07 -0500

    Fix broken tests with new UI

--- cypress/integration/auth.js
+++ cypress/integration/auth.js
@@ -7,7 +7,7 @@
 function typeCredentials(user) {
   cy.get("input[type=email]").click().type(user["email"]);
   cy.get("input[type=password]").click().type(user["password"]);
-  cy.findByRole('button', {name: /Iniciar/i}).click()
+  cy.findByRole("button", { name: /Iniciar/i }).click();
 }
 
 describe("Login", () => {
@@ -16,24 +16,48 @@ describe("Login", () => {
   });
 
   it("Logins admin correctly", () => {
-    typeCredentials({email:"admin@ontime.com",password:"ontime"})
-    cy.contains("administrador", { matchCase: false }).should("exist");
+    typeCredentials({ email: "admin@ontime.com", password: "ontime" });
+    cy.contains("administrador", { matchCase: false }).should("be.visible");
   });
 
   it("Logins employee correctly", () => {
-    typeCredentials({email:"employee@ontime.com",password:"ontime"})
-    cy.contains("empleado", { matchCase: false }).should("exist");
+    typeCredentials({ email: "employee@ontime.com", password: "ontime" });
+    cy.contains("empleado", { matchCase: false }).should("be.visible");
   });
 
   it("Logins employer correctly", () => {
-    typeCredentials({email:"employeer@ontime.com",password:"ontime"})
-    //Find camera button
-    cy.get(
-      '[style="background-color: rgb(3, 218, 196); bottom: 96px; box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 6px; opacity: 1; right: 24px; transform: scale(1);"] > .css-cursor-18t94o4'
-    ).should("exist");
-    //Find plus button
-    cy.get(
-      '[style="background-color: rgb(3, 218, 196); border-radius: 28px; bottom: 24px; box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 6px; opacity: 1; right: 24px; transform: scale(1);"] > .css-cursor-18t94o4'
-    ).should("exist");
+    typeCredentials({ email: "employeer@ontime.com", password: "ontime" });
+    //Find attendance button
+    cy.get(".r-pointerEvents-105ug2t > .css-cursor-18t94o4").should(
+      "be.visible"
+    );
+    cy.findAllByText(/sitio/i).should("be.visible");
+  });
+
+  it("Does not login with ID", () => {
+    typeCredentials({ email: "1098736511", password: "ontime" });
+    cy.findByText(/correo inválido/i).should("be.visible");
+  });
+
+  it("Asks all required inputs", () => {
+    cy.findByRole("button", { name: /Iniciar/i }).click();
+    cy.findAllByText(/requerido/i).should("be.visible");
+    cy.reload();
+    cy.findByRole("textbox", { name: /Correo/i })
+      .focus()
+      .blur();
+    cy.findAllByText(/requerido/i).should("be.visible");
+  });
+
+  it("Does not login with invalid credentials", () => {
+    typeCredentials({ email: "employeer@ontime.com", password: "abcd123" });
+    cy.findByText(/incorrectos/i).should("be.visible");
+  });
+
+  it("Preserves past login", () => {
+    typeCredentials({ email: "employee@ontime.com", password: "ontime" });
+    cy.contains("empleado", { matchCase: false }).should("be.visible");
+    cy.reload();
+    cy.contains("empleado", { matchCase: false }).should("be.visible");
   });
 });
