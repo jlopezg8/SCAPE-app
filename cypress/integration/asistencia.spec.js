abfaf390eacfdae2cd35b10c0336a01af30f83cf
Jose Gustavo Garcia <jggarciago@gmail.com>
2021-10-15 14:21:05 -0500

    Refactor employeeCRUD test and photo attempts

--- cypress/integration/asistencia.spec.js
+++ cypress/integration/asistencia.spec.js
@@ -1,24 +1,47 @@
-describe.skip("Register attendance", () => {
+describe("Register attendance", () => {
+  // login just once using API
+  let user;
+  before(() => {
+    cy.fetchEmployer().then((res) => {
+      user = res;
+    });
+  });
+
+  // but set the user before visiting the page
+  // so the app thinks it is already authenticated
   beforeEach(() => {
+    cy.setUser(user);
     cy.visit("/");
-    cy.get("input[type=email]").click().type("");
-    cy.get("input[type=password]").click().type("");
-    cy.get("[role=button]").click();
-    cy.wait(5000);
-    cy.get(
-      '[style="background-color: rgb(3, 218, 196); border-radius: 28px; bottom: 24px; box-shadow: rgba(0, 0, 0, 0.24) 0px 5px 6px; opacity: 1; right: 24px; transform: scale(1);"] > .css-cursor-18t94o4'
-    ).click();
+    cy.get(".r-pointerEvents-105ug2t > .css-cursor-18t94o4").click();
   });
-  it("Detects employee properly", () => {
-    //Select photo
+  describe("Detects employee properly and registers attendances", () => {
+    beforeEach(() => {
+      pickPhoto();
+    });
+    it.skip("Registers Entrance attendance", () => {
+      cy.findByText().should("be.visible");
+    });
+    it.skip("Registers Exit attendance", () => {
+      cy.pause();
+    });
+    it("Shows error because base64 is screwed :v", () => {
+      cy.findByText(/Base-64/i).should("be.visible");
+    });
   });
-  it("Registers Entrance attendance", () => {
-    //Select photo
-  });
-  it("Registers Exit attendance", () => {
-    //Select photo
-  });
-  it("Shows error on bad photo", () => {
-    //Select photo
+
+  it.skip("Shows error on bad photo", () => {
+    expect.hasAssertions();
   });
 });
+function pickPhoto() {
+  const falcao = "falcao.png";
+
+  cy.get(
+    ".r-alignSelf-1kihuf0 > :nth-child(1) > .r-cursor-1loqt21 > .css-view-1dbjc4n"
+  ).click();
+  cy.findByRole("menuitem", { name: /elegir una foto/i })
+    .click()
+    .then(() => {
+      cy.get('input[type="file"]').last().attachFile(falcao);
+    });
+}
