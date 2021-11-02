21a521f340e5142c33787a5956da86902d2a2077
Jose Gustavo Garcia <jggarciago@gmail.com>
2021-11-02 10:37:24 -0500

    Add integration tests and fix former test

--- cypress/integration/workplace.spec.js
+++ cypress/integration/workplace.spec.js
@@ -1,9 +1,32 @@
-//Login Employee offscreen
 describe("Workplace list", () => {
-  it.skip("Shows at least one workplace", () => {
+  let user;
+  before(() => {
+    cy.fetchEmployer().then((res) => {
+      user = res;
+    });
+  });
+  beforeEach(() => {
+    cy.setUser(user);
+    cy.visit("/employer");
+  });
+  it("shows at least one workplace", () => {
+    cy.findByText(/TestWorkPlace/).should("be.visible");
+  });
+  it("message on invalid workplace", () => {
+    cy.visit("/employer/workplace/0");
+    cy.findByText(/No encontrado/i).should("be.visible");
+    cy.findByText(/Radamel Falcao/, { timeout: 10000 }).should("not.exist");
+  });
+  it("shows workplace info", () => {
+    cy.visit("/employer/workplace/3");
+    cy.findByText(/Empresa de Metales/).should("be.visible");
+    cy.findByText(/Malteria/).should("be.visible");
+    cy.findByText(/Zona industrial de Manizales/).should("be.visible");
+  });
+  it.skip("shows message on no workplaces", () => {
     expect.hasAssertions();
   });
-  it.skip("Shows message on no workplaces", () => {
+  /*it.skip("shows error on no connection", () => {
     expect.hasAssertions();
-  });
+  });*/
 });
